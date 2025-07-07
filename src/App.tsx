79603508b03e152addf988b472538ad724a41385
oheunbee@users.noreply.github.com <dhdmsql1324@gamil.com>
2025-07-07 14:48:46 +0900

    App.tsx 리디렉션 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
@@ -12,12 +12,17 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Admin 경로 - Layout 없이 */}
         <Route path="/Admin/login" element={<Login />} />
         <Route path="/Admin/dashboard" element={<Dashboard />} />
-
-        {/* 일반 사용자용 Layout 라우트 */}
         <Route element={<Layout />}>
+          <Route 
+            path="/company" 
+            element={<Navigate to="/about" replace />} 
+          />
+          <Route 
+            path="/products" 
+            element={<Navigate to="/Contents" replace />} 
+          />
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/Contents" element={<Contents />} />
